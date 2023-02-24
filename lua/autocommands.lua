e4c4d482d11918936d331239b059a854fe5e947f
Thiago Marques <megathiago@gmail.com>
2023-02-24 14:42:42 -0300

    Created local variable for autocmd

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,8 +1,9 @@
 ---@diagnostic disable: undefined-global
 local commands_group = vim.api.nvim_create_augroup("CustomCommands", { clear = true })
+local autocmd = vim.api.nvim_create_autocmd
 
 -- Highlight the region on yank
-vim.api.nvim_create_autocmd("TextYankPost", {
+autocmd("TextYankPost", {
 	group = commands_group,
 	callback = function()
 		vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
@@ -10,27 +11,27 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- Insert mode when create new file
-vim.api.nvim_create_autocmd("BufNewFile", {
+autocmd("BufNewFile", {
 	group = commands_group,
 	command = 'startinsert | :move .-1'
 })
 
 -- Set autoread when a file is changed from the outside
 vim.opt.autoread = true
-vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
+autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
 	group = commands_group,
 	command = 'checktime'
 })
 
 -- Auto reload configs on save
-vim.api.nvim_create_autocmd("BufWritePost", {
+autocmd("BufWritePost", {
 	group = commands_group,
 	pattern = { 'autocommands.lua', 'colorscheme.lua', 'keybinds.lua', 'options.lua' },
 	command = 'source <afile>'
 })
 
 -- Auto reload plugins on save
-vim.api.nvim_create_autocmd("BufWritePost", {
+autocmd("BufWritePost", {
 	group = commands_group,
 	pattern = 'plugins.lua',
 	command = 'source <afile> | CocUpdateSync | PackerSync '
